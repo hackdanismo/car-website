dc9fbbc584276762a55a69531f28d3ad42939920
hackdanismo <office@justtoconfirm.co.uk>
2026-07-07 18:52:00 +0100

    feat: update the index.ts file in the sanity/schemaTypes directory to add the newsArticle schema

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,6 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from "sanity"
+import { newsArticle } from "./newsArticle"
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [newsArticle],
 }
